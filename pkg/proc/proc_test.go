002a3f00447a65c4dcb677eb4a5a699cf7b98293
Dimitris Papastamos <dimitris@undo.io>
2019-05-28 19:00:04 +0100

    Support for Undo record/replay backend.
    * Update the documentation to mention UDB or LiveRecorder alongside rr.
    * Add makefile support for testing the Undo backend.
    * Add DLV_RECORD_REPLAY_BACKEND environment variable which overrides
      the record-replay backend, for use in GoLand.
    * Implement checkpoints as a map from checkpoint number to time.
    * Suppress resume call after restart.
    * Implement "when" command using "get_time" serial command.
    * Implement call injection using "set_debuggee_volatile".
    * Implement "restart" using "goto_time".
    * Enable or disable Undo backend appropriately in test cases.

--- pkg/proc/proc_test.go
+++ pkg/proc/proc_test.go
@@ -94,19 +94,23 @@ func withTestProcessArgs(name string, t testing.TB, wd string, args []string, bu
 	}
 	fixture := protest.BuildFixture(t, name, buildFlags)
 
-	grp := startTestProcessArgs(fixture, t, wd, args)
+	grp, recording := startTestProcessArgs(fixture, t, wd, args)
 
 	defer func() {
 		grp.Detach(true)
+		if recording != "" {
+			os.Remove(recording)
+		}
 	}()
 
 	fn(grp.Selected, grp, fixture)
 }
 
-func startTestProcessArgs(fixture protest.Fixture, t testing.TB, wd string, args []string) *proc.TargetGroup {
+func startTestProcessArgs(fixture protest.Fixture, t testing.TB, wd string, args []string) (*proc.TargetGroup, string) {
 	var grp *proc.TargetGroup
 	var err error
 	var tracedir string
+	var recording string
 
 	switch testBackend {
 	case "native":
@@ -118,13 +122,19 @@ func startTestProcessArgs(fixture protest.Fixture, t testing.TB, wd string, args
 		t.Log("recording")
 		grp, tracedir, err = gdbserial.RecordAndReplay(append([]string{fixture.Path}, args...), wd, true, []string{}, "", proc.OutputRedirect{}, proc.OutputRedirect{})
 		t.Logf("replaying %q", tracedir)
+	case "undo":
+		protest.MustHaveRecordingAllowed(t)
+		t.Log("recording")
+		grp, recording, err = gdbserial.UndoRecordAndReplay(append([]string{fixture.Path}, args...), wd, true, []string{}, "", proc.OutputRedirect{}, proc.OutputRedirect{})
+		t.Logf("replaying")
 	default:
 		t.Fatal("unknown backend")
 	}
 	if err != nil {
 		t.Fatal("Launch():", err)
 	}
-	return grp
+
+	return grp, recording
 }
 
 func getRegisters(p *proc.Target, t *testing.T) proc.Registers {
@@ -988,7 +998,7 @@ func evalVariableOrError(p *proc.Target, symbol string) (*proc.Variable, error) 
 	var scope *proc.EvalScope
 	var err error
 
-	if testBackend == "rr" {
+	if testBackend == "rr" || testBackend == "undo" {
 		var frame proc.Stackframe
 		frame, err = findFirstNonRuntimeFrame(p)
 		if err == nil {
@@ -2334,7 +2344,7 @@ func TestIssue594(t *testing.T) {
 		assertNoError(grp.Continue(), t, "Continue()")
 		var f string
 		var ln int
-		if testBackend == "rr" {
+		if testBackend == "rr" || testBackend == "undo" {
 			frame, err := findFirstNonRuntimeFrame(p)
 			assertNoError(err, t, "findFirstNonRuntimeFrame")
 			f, ln = frame.Current.File, frame.Current.Line
@@ -2476,7 +2486,7 @@ func TestAttachDetach(t *testing.T) {
 			return
 		}
 	}
-	if testBackend == "rr" {
+	if testBackend == "rr" || testBackend == "undo" {
 		return
 	}
 	var buildFlags protest.BuildFlags
@@ -2688,7 +2698,7 @@ func TestIssue871(t *testing.T) {
 
 		var scope *proc.EvalScope
 		var err error
-		if testBackend == "rr" {
+		if testBackend == "rr" || testBackend == "undo" {
 			var frame proc.Stackframe
 			frame, err = findFirstNonRuntimeFrame(p)
 			if err == nil {
@@ -5423,7 +5433,7 @@ func TestWaitForAttach(t *testing.T) {
 			return
 		}
 	}
-	if testBackend == "rr" {
+	if testBackend == "rr" || testBackend == "undo" {
 		return
 	}
 
@@ -5752,7 +5762,10 @@ func TestChainedBreakpoint(t *testing.T) {
 
 		t.Logf("=== Restart ===")
 
-		grp2 := startTestProcessArgs(fixture, t, ".", []string{})
+		grp2, recording := startTestProcessArgs(fixture, t, ".", []string{})
+		defer func () {
+			os.Remove(recording)
+		}()
 		proc.Restart(grp2, grp, func(lbp *proc.LogicalBreakpoint, err error) {
 			t.Fatalf("discarded logical breakpoint %v: %v", lbp, err)
 		})
