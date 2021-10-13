f8dffdfa3da1d95f7a6830aceab6e4bd66dede72
Gareth Rees <grees@undo.io>
2021-10-13 13:25:31 +0100

    Improve coverage of record-replay backends by allowing recording.

--- pkg/proc/variables_test.go
+++ pkg/proc/variables_test.go
@@ -1076,6 +1076,7 @@ func TestConstants(t *testing.T) {
 		// Not supported on 1.9 or earlier
 		t.Skip("constants added in go 1.10")
 	}
+	protest.AllowRecording(t)
 	withTestProcess("consts", t, func(p *proc.Target, fixture protest.Fixture) {
 		assertNoError(p.Continue(), t, "Continue")
 		for _, testcase := range testcases {
@@ -1087,6 +1088,7 @@ func TestConstants(t *testing.T) {
 }
 
 func TestIssue1075(t *testing.T) {
+	protest.AllowRecording(t)
 	withTestProcess("clientdo", t, func(p *proc.Target, fixture protest.Fixture) {
 		setFunctionBreakpoint(p, t, "net/http.(*Client).Do")
 		assertNoError(p.Continue(), t, "Continue()")
@@ -1360,6 +1362,7 @@ func testCallFunction(t *testing.T, p *proc.Target, tc testCaseCallFunction) {
 
 func TestIssue1531(t *testing.T) {
 	// Go 1.12 introduced a change to the map representation where empty cells can be marked with 1 instead of just 0.
+	protest.AllowRecording(t)
 	withTestProcess("issue1531", t, func(p *proc.Target, fixture protest.Fixture) {
 		assertNoError(p.Continue(), t, "Continue()")
 
